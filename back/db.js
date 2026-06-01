031831776db55e78511ce70b3f9cc03ca4d1d2a2
juio12 <gtase08_@naver.com>
2026-06-01 10:25:42 +0900

    DB 환경변수 분리

--- back/db.js
+++ back/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "0000",
-    database: "tonez"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "0000",
+    database: process.env.DB_NAME || "tonez"
 });
 
 db.connect(function (err) {
