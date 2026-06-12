0fe8f118ba861e1cb488614d572b5b6647980b4a
juio12 <gtase08_@naver.com>
2026-06-12 11:23:27 +0900

    결과 페이지 헤더에 마이페이지 버튼 추가

--- front/src/app/result/[tone]/page.js
+++ front/src/app/result/[tone]/page.js
@@ -79,6 +79,10 @@ function ResultHeader({ data, userName, tone }) {
         <div className="absolute left-1/2 top-[55px] z-10 -translate-x-1/2">
           <img src={getSeasonResultImg(tone)} alt="퍼스널컬러 진단 결과" className="h-auto w-[clamp(260px,26vw,400px)]" />
         </div>
+
+        <a href="/mypage" className="ml-auto transition hover:opacity-80">
+          <img src="/img/MyPage_button.png" alt="My Page" className="h-auto w-[clamp(100px,10vw,160px)]" />
+        </a>
       </div>
 
       <img
